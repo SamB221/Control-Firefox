928f1fc165809fa8462fffa929525708352b6271
Sam Bardsley <bardsls@wwu.edu>
2024-04-01 01:13:00 -0700

    cleaned up options

--- js/options.js
+++ js/options.js
@@ -1,20 +1,21 @@
 const numOfOptions = 4;
 const checks = new Array(numOfOptions).fill(false);
-initialize();
 
 function initialize() {
     chrome.storage.sync.get(null, function(data) {
         for (let i = 0; i < numOfOptions; i++) {
             if (data[i+""] !== undefined) {
                 checks[i] = data[i+""];
+                /*document.getElementsByClassName(".slider").style.transition = "0s";*/
                 document.getElementById(i+"").checked = checks[i];
+                /*document.getElementsByClassName(".slider").style.transition = ".3s";*/
             }
         }
         updateImage();
     });
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("DOMContentLoaded", initialize);
 document.querySelector("form").addEventListener("change", saveOptions);
 
 
@@ -31,19 +32,6 @@ function saveOptions(e) {
     chrome.storage.sync.set(data);
     
     e.preventDefault();
-}
-  
-function restoreOptions() {
-    /*function setCurrentChoice(result) {
-      document.querySelector("#color").value = result.color || "blue";
-    }
-  
-    function onError(error) {
-      console.log(`Error: ${error}`);
-    }
-  
-    let getting = browser.storage.sync.get("color");
-    getting.then(setCurrentChoice, onError);*/
 }
 
 function removeSheet() {
@@ -64,8 +52,4 @@ function updateImage() {
     document.getElementById("c1").style.borderColor = "#272727";
     document.getElementById("c1").style.backgroundImage = 'url("../icons/control_grey.svg")';
     document.getElementById("title").style.color = "#272727";
-}
-
-function toggleSlider(i) {
-    document.getElementById()
 }
